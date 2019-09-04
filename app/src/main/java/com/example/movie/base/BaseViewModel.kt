0b16b85d4671f34ff12bf19b2b77b9d8f54ea373
wokrey <wokrey@gmail.com>
2019-09-05 01:14:27 +0600

    base coroutine vm

---
+++ app/src/main/java/com/example/movie/base/BaseViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.movie.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Created by wokrey@gmail.com on 9/5/19.
+ * It's not wokrey, if the code smells bad. Somebody set me up.
+ */
+open class BaseViewModel(
+    protected val uiContext: CoroutineContext = Dispatchers.Main,
+    protected val bg: CoroutineContext = Dispatchers.IO
+): ViewModel(), CoroutineScope {
+
+    protected val progressBarStatusLD = MutableLiveData<Boolean>()
+    protected val errorLD = MutableLiveData<Exception>()
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext by lazy {
+        uiContext + job
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+    //getters
+    fun getErrorLD(): LiveData<Exception> = errorLD
+
+    fun getProgressBarStatusLD(): LiveData<Boolean> = progressBarStatusLD
+}
