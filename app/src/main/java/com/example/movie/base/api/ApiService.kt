55b44f955f24f18bc1440acee985dfef6f465d9d
wokrey <wokrey@gmail.com>
2019-09-05 21:44:55 +0600

    add popular api method

--- app/src/main/java/com/example/movie/base/api/ApiService.kt
+++ app/src/main/java/com/example/movie/base/api/ApiService.kt
@@ -1,8 +1,19 @@
 package com.example.movie.base.api
 
+import com.example.movie.main.films.data.model.PopularResponse
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * Created by wokrey@gmail.com on 9/4/19.
  * It's not wokrey, if the code smells bad. Somebody set me up.
  */
 interface ApiService {
+
+    @GET("movie/popular")
+    fun getPopular(
+        @Query("language") language: String,
+        @Query("page") page: Int
+    ): Deferred<PopularResponse>
 }
