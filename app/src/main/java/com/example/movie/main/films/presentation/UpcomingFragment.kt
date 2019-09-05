c0b69f5df57ed5a56b798ea4cdd2067986d347ac
wokrey <wokrey@gmail.com>
2019-09-06 00:56:51 +0600

    load data

--- app/src/main/java/com/example/movie/main/films/presentation/UpcomingFragment.kt
+++ app/src/main/java/com/example/movie/main/films/presentation/UpcomingFragment.kt
@@ -6,9 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.movie.BR
 import com.example.movie.R
 import com.example.movie.base.BaseFragment
-import com.example.movie.databinding.FragmentSoonOnBinding
+import com.example.movie.base.RecyclerBindingAdapter
+import com.example.movie.databinding.FragmentUpcomingBinding
+import com.example.movie.main.films.data.model.UpcomingMovie
+import com.example.movie.main.films.domain.UpcomingViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class UpcomingFragment : BaseFragment() {
@@ -19,14 +26,30 @@ class UpcomingFragment : BaseFragment() {
         fun newInstance() = UpcomingFragment()
     }
 
-    lateinit var binding: FragmentSoonOnBinding
+    lateinit var binding: FragmentUpcomingBinding
+    lateinit var adapter: RecyclerBindingAdapter<UpcomingMovie>
+
+    private val viewModel: UpcomingViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = RecyclerBindingAdapter(
+            holderLayout = R.layout.item_upcoming,
+            variableId = BR.upcoming,
+            context = context!!
+        )
+        viewModel.loadUpcomingMovies()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_upcoming, container, false)
-
+        binding.rvUpcoming.adapter = adapter
+        binding.rvUpcoming.layoutManager = StaggeredGridLayoutManager( 2, RecyclerView.VERTICAL)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
         return binding.root
     }
 
