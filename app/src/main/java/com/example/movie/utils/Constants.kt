c73295a02e0523e3568479b223e9d4ac758bc909
wokrey <wokrey@gmail.com>
2019-09-05 21:45:11 +0600

    constants file

---
+++ app/src/main/java/com/example/movie/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.movie.utils
+
+/**
+ * Created by wokrey@gmail.com on 9/5/19.
+ * It's not wokrey, if the code smells bad. Somebody set me up.
+ */
+object Constants {
+    const val RUSSIAN_LANGUAGE ="ru-RUS"
+    const val ENGLISH_LANGUAGE ="en-US"
+}
